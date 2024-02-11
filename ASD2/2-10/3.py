469819920c1630157d37b5f17ba53d36ac27a919
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-11 18:33:07 +0200

    fix

--- ASD2/2-10/3.py
+++ ASD2/2-10/3.py
@@ -1,4 +1,4 @@
-def read_adjacency_matrix(file_path):
+def read_matrix(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
         matrix = [list(map(int, line.strip().split())) for line in lines]
@@ -9,7 +9,7 @@ def write_results(file_path, connectcomp):
         for i, component in enumerate(connectcomp):
             f.write(f"Компонент связанности {i+1}: {component}\n")
 
-def breadth_first_search(adj_matrix, start, visited):
+def bfs(adj_matrix, start, visited):
     queue = [start]
     visited[start] = True
 
@@ -28,7 +28,7 @@ def find_connect(adj_matrix):
     for vertex in range(num_vertices):
         if not visited[vertex]:
             component = []
-            breadth_first_search(adj_matrix, vertex, visited)
+            bfs(adj_matrix, vertex, visited)
             for i, v in enumerate(visited):
                 if v:
                     component.append(i)
@@ -39,9 +39,9 @@ def find_connect(adj_matrix):
 
 
 input_file = 'input.txt'
-output_file = 'output.txt'
+output_file = 'output_3.txt'
 
-adj_matrix = read_adjacency_matrix(input_file)
+adj_matrix = read_matrix(input_file)
 connectivity_components = find_connect(adj_matrix)
 write_results(output_file, connectivity_components)
 
--- ASD2/2-10/4.py
+++ ASD2/2-10/4.py
@@ -1,24 +1,41 @@
-MAXN = 1e5
-g = [[] for _ in range(MAXN)]
-used = [False] * MAXN
-comp = []
+def read_matrix(input_file):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+        graph = [[int(val) for val in line.split()] for line in lines]
+    return graph
 
-def dfs(v):
-    used[v] = True
-    comp.append(v)
-    for to in g[v]:
-        if not used[to]:
-            dfs(to)
+def write_results(components, output_file):
+    with open(output_file, 'w') as f:
+        for i, component in enumerate(components):
+            vertex = ','.join(map(str,[vertex+1 for vertex in component]))
+            f.write(f"Компонент связанности {i+1}: {vertex}\n")
 
-def find_comps(n):
-    for i in range(n):
-        used[i] = False
-    for i in range(n):
-        if not used[i]:
-            comp.clear()
-            dfs(i)
- 
-            print("Component:", end="")
-            for j in range(len(comp)):
-                print(' ', comp[j], end="")
-            print()
+
+def dfs(graph, visited, v, component):
+    visited[v] = True
+    component.append(v)
+    for i in range(len(graph)):
+        if graph[v][i] == 1 and not visited[i]:
+            dfs(graph, visited, i, component)
+
+def find_connect(graph):
+    num_vertices = len(graph)
+    visited = [False] * num_vertices
+    components = []
+
+    for v in range(num_vertices):
+        if not visited[v]:
+            component = []
+            dfs(graph, visited, v, component)
+            components.append(component)
+
+    return components
+
+
+
+input_file = "input.txt"
+output_file = "output_4.txt"
+
+adjacency_matrix = read_matrix(input_file)
+connectivity_components = find_connect(adjacency_matrix)
+write_results(connectivity_components, output_file)
