aa400aa0fcc1429d2940877ab6a4a53329ade3dc
SpaghettiSauce <dontstarve221@gmail.com>
2024-02-24 11:30:38 +0200

    5 final

--- ASD2/2-10/5.py
+++ ASD2/2-10/5.py
@@ -1,59 +1,50 @@
-def read_matrix(input_file):
-    with open(input_file, 'r') as file:
+def read_matrix(filename):
+    with open(filename, 'r') as file:
         lines = file.readlines()
-        graph = [[int(val) for val in line.split()] for line in lines]
-    return graph
+        return [[int(x) for x in line.split()] for line in lines]
 
-def write_results(filename, components):
-    with open(filename, 'w') as f:
-        f.write(f"Колво сильно связанных комп: {len(components)}\n")
-        for component in components:
+def write_results(components, output_file):
+    with open(output_file, 'w') as f:
+        for i, component in enumerate(components):
             vertex = ','.join(map(str,[vertex+1 for vertex in component]))
-            f.write(" ".join(map(str, vertex)) + "\n")
+            f.write(f"Компонент связанности {i+1}: {vertex}\n")
 
-def transpose(graph):
-    return [[graph[j][i] for j in range(len(graph))] for i in range(len(graph[0]))]
+def transpose_graph(graph):
+    return [[graph[j][i] for j in range(len(graph))] for i in range(len(graph))]
 
-def dfs(graph, v, visited, stack):
-    visited[v] = True
-    for neighbor in range(len(graph)):
-        if graph[v][neighbor] == 1 and not visited[neighbor]:
-            dfs(graph, neighbor, visited, stack)
-    stack.append(v)
+def dfs(graph, vertex, visited, stack):
+    visited[vertex] = True
+    for i in range(len(graph)):
+        if graph[vertex][i] and not visited[i]:
+            dfs(graph, i, visited, stack)
+    stack.append(vertex)
 
-def dfs_util(graph, v, visited, component):
-    visited[v] = True
-    component.append(v)
-    for neighbor in range(len(graph)):
-        if graph[v][neighbor] == 1 and not visited[neighbor]:
-            dfs_util(graph, neighbor, visited, component)
-
-def strongly_connected_components(graph):
-    num_vertices = len(graph)
-    visited = [False] * num_vertices
+def kosaraju(graph):
+    n = len(graph)
+    visited = [False] * n
     stack = []
 
-    for v in range(num_vertices):
-        if not visited[v]:
-            dfs(graph, v, visited, stack)
+    for i in range(n):
+        if not visited[i]:
+            dfs(graph, i, visited, stack)
 
-    transposed_graph = transpose(graph)
-    visited = [False] * num_vertices
-    components = []
+    transposed_graph = transpose_graph(graph)
+    visited = [False] * n
+    scc_list = []
 
     while stack:
-        v = stack.pop()
-        if not visited[v]:
+        vertex = stack.pop()
+        if not visited[vertex]:
             component = []
-            dfs_util(transposed_graph, v, visited, component)
-            components.append(component)
+            dfs(transposed_graph, vertex, visited, component)
+            scc_list.append(component)
 
-    return components
+    return scc_list
 
 
-input_file = "input.txt"
-output_file = "output_5.txt"
+input_file = 'input.txt'
+output_file = 'output_5.txt'
 
 graph = read_matrix(input_file)
-components = strongly_connected_components(graph)
-write_results(output_file, components)
+scc_list = kosaraju(graph)
+write_results(scc_list, output_file)
