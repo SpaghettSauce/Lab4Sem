c37bc404a9f5e3c651735438e6f79080b22ce027
SpaghettiSauce <dontstarve221@gmail.com>
2024-03-12 20:04:49 +0200

    Update Dis-2_1.py

--- Avt&Dis/Dis-2_1.py
+++ Avt&Dis/Dis-2_1.py
@@ -1,9 +1,11 @@
 from itertools import permutations
 
-word = "combinatorics"
+'''1'''
+word = "комбинаторика"
 letters = list(word)
 unique_letters = set(letters)
-permutations = permutations(unique_letters, 5)
+permutations = permutations(unique_letters, 4)
 unique_words = set([''.join(p) for p in permutations])
 
 print("Number of different 5-letter words:", len(unique_words))
+
