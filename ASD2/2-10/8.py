b63f1ac1b69562c72dd7a7cd8cac4c32a3ef61ac
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-03-25 00:15:36 +0200

    8-9

---
+++ ASD2/2-10/8.py
@@ -0,0 +1,53 @@
+INF = float('inf')
+
+def read_matrix(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        graph = []
+        for line in lines:
+            graph.append(list(map(int, line.split())))
+    return graph
+
+def write_results(filename, result):
+    with open(filename, 'w') as file:
+        for dist in result:
+            file.write(str(dist) + '\n')
+
+def dijkstra(graph, start):
+    n = len(graph)
+    dist = [INF] * n
+    dist[start] = 0
+    visited = [False] * n
+
+    for _ in range(n):
+        u = min_distance(dist, visited)
+        visited[u] = True
+
+        for v in range(n):
+            if graph[u][v] and not visited[v] and dist[u] != INF and dist[u] + graph[u][v] < dist[v]:
+                dist[v] = dist[u] + graph[u][v]
+
+    return dist
+
+def min_distance(dist, visited):
+    min_dist = INF
+    min_index = -1
+    for v in range(len(dist)):
+        if not visited[v] and dist[v] < min_dist:
+            min_dist = dist[v]
+            min_index = v
+    return min_index
+
+
+
+
+
+
+input_file = "input.txt"  
+output_file = "output_8.txt"  
+
+graph = read_matrix(input_file)
+start_vertex = 0  
+
+shortest_distances = dijkstra(graph, start_vertex)
+write_results(output_file, shortest_distances)
---
+++ ASD2/2-10/9.py
@@ -0,0 +1,42 @@
+INF = float('inf')
+
+def bellman_ford(graph, start):
+    n = len(graph)
+    dist = [INF] * n
+    dist[start] = 0
+
+    for _ in range(n - 1):
+        for u in range(n):
+            for v in range(n):
+                if graph[u][v] != 0 and dist[u] != INF and dist[u] + graph[u][v] < dist[v]:
+                    dist[v] = dist[u] + graph[u][v]
+
+    for u in range(n):
+        for v in range(n):
+            if graph[u][v] != 0 and dist[u] != INF and dist[u] + graph[u][v] < dist[v]:
+                return "Graph contains negative weight cycle"
+
+    return dist
+
+def read_graph_from_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        graph = []
+        for line in lines:
+            graph.append(list(map(int, line.split())))
+    return graph
+
+def write_result_to_file(filename, result):
+    with open(filename, 'w') as file:
+        for dist in result:
+            file.write(str(dist) + '\n')
+
+
+input_file = "input.txt"  
+output_file = "output_9.txt" 
+
+graph = read_graph_from_file(input_file)
+start_vertex = 0  
+
+shortest_distances = bellman_ford(graph, start_vertex)
+write_result_to_file(output_file, shortest_distances)
