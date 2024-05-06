e265123e7c202ef5f365bec6da48948359f70438
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-05-06 21:41:13 +0200

    fin

--- Avt&Dis/DIS-2/Dis-2_1.py
+++ Avt&Dis/DIS-2/Dis-2_1.py
@@ -1,9 +1,10 @@
-word = "Комбинатoрuкa"
+word =['к','о','м','б','и','н','а','т','о','р','и','к','а',]
 endW = set()
 
 for i in range(len(word)):
     for j in range(len(word)):
         for q in range(len(word)):
             for c in range(len(word)):
-                four = word[i] + word[j] + word[q] + word[c]
+                endW.add( word[i] + word[j] + word[q] + word[c])
                 
+print (len(endW))
--- Avt&Dis/DIS-2/Dis-2_6.py
+++ Avt&Dis/DIS-2/Dis-2_6.py
@@ -22,3 +22,22 @@ def count_paths(rows, cols):
 rectangle_rows = 15
 rectangle_cols = 14
 print("Кол-во разных путе:", count_paths(rectangle_rows, rectangle_cols))
+
+def count_paths(rows, cols):
+    paths = [[0] * (cols + 1) for _ in range(rows + 1)]
+    paths[0][0] = 1
+
+    for i in range(rows + 1):
+        for j in range(cols + 1):
+            if i != 0 or j != 0:
+                # Summing paths from top and left cells
+                paths[i][j] = 0
+                if j >= 2:
+                    paths[i][j] += paths[i][j - 2]
+                paths[i][j] += paths[i - 1][j]
+
+    return paths[rows][cols]
+
+rectangle_rows = 15
+rectangle_cols = 14
+print("Number of different paths:", count_paths(rectangle_rows, rectangle_cols))
