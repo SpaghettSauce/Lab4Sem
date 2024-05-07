b05bb321f85f5e5611152e1bcb11affd76604ae1
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-05-07 16:31:21 +0200

    Update Dis-2_6.py

--- Avt&Dis/DIS-2/Dis-2_6.py
+++ Avt&Dis/DIS-2/Dis-2_6.py
@@ -1,43 +1,27 @@
-def count_paths(rows, cols):
-    paths = [[0] * (cols + 1) for _ in range(rows + 1)]
-    paths[0][0] = 1
-
-    for i in range(rows + 1):
-        for j in range(cols + 1):
-            # Если мы не в начале
-            if i != 0 or j != 0:
-                # если мы слева сверху, то путь 1
-                if i == 0 and j == 0:
-                    paths[i][j] = 1
-                else:
-                    # проверяем если предыдущий ход был вертикальным 
-                    if j >= 2:
-                        paths[i][j] += paths[i][j - 2]
-                    # проверяем если предыдущий ход был вертикальным
-                    if i >= 1:
-                        paths[i][j] += paths[i - 1][j]
-
-    return paths[rows][cols]
-
-rectangle_rows = 15
-rectangle_cols = 14
-print("Кол-во разных путе:", count_paths(rectangle_rows, rectangle_cols))
-
-def count_paths(rows, cols):
-    paths = [[0] * (cols + 1) for _ in range(rows + 1)]
-    paths[0][0] = 1
-
-    for i in range(rows + 1):
-        for j in range(cols + 1):
-            if i != 0 or j != 0:
-           
-                paths[i][j] = 0
-                if j >= 2:
-                    paths[i][j] += paths[i][j - 2]
-                paths[i][j] += paths[i - 1][j]
-
-    return paths[rows][cols]
-
-rectangle_rows = 15
-rectangle_cols = 14
-print("Number of different paths:", count_paths(rectangle_rows, rectangle_cols))
+size_ver = 15size_hor = 14
+matrix = [[0 for x in range(size_hor)] for y in range(size_ver)]
+matrix[0][0] = 1# for count in range(1, size_ver):
+#     i = count#     j = 0
+#     while i >= 0 and j < size_hor:#         if i != 0:
+#             matrix[i][j] = matrix[i -1][j]#         if j != 0:
+#             matrix[i][j] += matrix[i][j - 1]#         i -= 1
+#         j += 1#         if i== 0 and j == 1:
+#             matrix[i][j] = 1# for count in range(1, size_hor):
+#     i = size_ver - 1#     j = count
+#     while j < size_hor and i >= 1:#         if i != 0:
+#             matrix[i][j] = matrix[i - 1][j]#         if j != 0:
+#             matrix[i][j] += matrix[i][j - 1]#         i -= 1
+#         j += 1
+# Случай когда два вертикальный участка не могут быть подрядfor count in range(1, size_ver):
+    i = count    j = 0
+    while i >= 0 and j < size_hor:        if i != 0:
+            matrix[i][j] = matrix[i -1][j]        if j != 0 and i != 0:
+            matrix[i][j] += matrix[i - 1][j - 1]        i -= 1
+        j += 1        if i== 0 and j == 1:
+            matrix[i][j] = 1
+for count in range(1, size_hor):    i = size_ver - 1
+    j = count    while j < size_hor and i >= 1:
+        if i != 0:            matrix[i][j] = matrix[i - 1][j]
+        if j != 0 and i != 0:            matrix[i][j] += matrix[i - 1][j - 1]
+        i -= 1        j += 1
+print(str(matrix[size_ver - 1][size_hor - 1]))
