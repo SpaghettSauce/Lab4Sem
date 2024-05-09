664aa95d3538d3c09e864bb1e47a050a08b7f3f0
Даня Ходосевич <114701605+SpaghettSauce@users.noreply.github.com>
2024-05-09 17:46:55 +0200

    Update Dis-2_6.py

--- Avt&Dis/DIS-2/Dis-2_6.py
+++ Avt&Dis/DIS-2/Dis-2_6.py
@@ -1,27 +1,51 @@
-size_ver = 15size_hor = 14
+size_ver = 15
+size_hor = 14
 matrix = [[0 for x in range(size_hor)] for y in range(size_ver)]
-matrix[0][0] = 1# for count in range(1, size_ver):
-#     i = count#     j = 0
-#     while i >= 0 and j < size_hor:#         if i != 0:
-#             matrix[i][j] = matrix[i -1][j]#         if j != 0:
-#             matrix[i][j] += matrix[i][j - 1]#         i -= 1
-#         j += 1#         if i== 0 and j == 1:
-#             matrix[i][j] = 1# for count in range(1, size_hor):
-#     i = size_ver - 1#     j = count
-#     while j < size_hor and i >= 1:#         if i != 0:
-#             matrix[i][j] = matrix[i - 1][j]#         if j != 0:
-#             matrix[i][j] += matrix[i][j - 1]#         i -= 1
-#         j += 1
-# Случай когда два вертикальный участка не могут быть подрядfor count in range(1, size_ver):
-    i = count    j = 0
-    while i >= 0 and j < size_hor:        if i != 0:
-            matrix[i][j] = matrix[i -1][j]        if j != 0 and i != 0:
-            matrix[i][j] += matrix[i - 1][j - 1]        i -= 1
-        j += 1        if i== 0 and j == 1:
+matrix[0][0] = 1
+
+for count in range(1, size_ver):
+    i = count   
+    j = 0
+    while i >= 0 and j < size_hor:
+        if i != 0:
+            matrix[i][j] = matrix[i -1][j]        
+        if j != 0:
+            matrix[i][j] += matrix[i][j - 1]       
+        i -= 1
+        j += 1        
+        if i== 0 and j == 1:
             matrix[i][j] = 1
-for count in range(1, size_hor):    i = size_ver - 1
-    j = count    while j < size_hor and i >= 1:
-        if i != 0:            matrix[i][j] = matrix[i - 1][j]
-        if j != 0 and i != 0:            matrix[i][j] += matrix[i - 1][j - 1]
-        i -= 1        j += 1
+for count in range(1, size_hor):
+    i = size_ver - 1     
+    j = count
+    while j < size_hor and i >= 1:         
+        if i != 0:
+            matrix[i][j] = matrix[i - 1][j]         
+        if j != 0:
+            matrix[i][j] += matrix[i][j - 1]       
+        i -= 1
+        j += 1
+# Случай 
+for count in range(1, size_ver):
+    i = count    
+    j = 0
+    while i >= 0 and j < size_hor:        
+        if i != 0:
+            matrix[i][j] = matrix[i -1][j]        
+        if j != 0 and i != 0:
+            matrix[i][j] += matrix[i - 1][j - 1]        
+        i -= 1
+        j += 1        
+        if i== 0 and j == 1:
+            matrix[i][j] = 1
+for count in range(1, size_hor):    
+    i = size_ver - 1
+    j = count    
+    while j < size_hor and i >= 1:
+        if i != 0:            
+            matrix[i][j] = matrix[i - 1][j]
+        if j != 0 and i != 0:           
+            matrix[i][j] += matrix[i - 1][j - 1]
+        i -= 1        
+        j += 1
 print(str(matrix[size_ver - 1][size_hor - 1]))
